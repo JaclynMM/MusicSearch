ac2a997d5305d643a12731223b42b7043141ba72
Jaclyn Mullin <jaclyn@Apples-MacBook-Pro.local>
2017-11-06 19:42:49 -0500

    Admin Setup

--- musicians/bands/admin.py
+++ musicians/bands/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Artist
+from .models import Artist, Band, Album
 
 admin.site.register(Artist)
+admin.site.register(Band)
+admin.site.register(Album)
+

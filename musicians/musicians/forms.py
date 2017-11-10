fb9783dd2da402c6168ec5e517ef01e5af953a19
Jaclyn Mullin <jaclyn.mullin@gmail.com>
2017-11-10 17:54:20 -0500

    Still working out views

--- musicians/musicians/forms.py
+++ musicians/musicians/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class search_form(forms.Form):
-    name = forms.CharField()
+    artist_name = forms.CharField(label='Artist', max_length=100)
+    band_name = forms.CharField(label='Band', max_length=100)
--- musicians/musicians/views.py
+++ musicians/musicians/views.py
@@ -5,10 +5,15 @@ from . import forms
 
 
 def home(request):
-    return render(request, 'index.html')
-    # return HttpResponse("<h1> Beyond the Band </h1>")
+    # return render(request, 'index.html')
+    return HttpResponse("<h1> Beyond the Band </h1>")
 
 
-def search_form_view(request):
-    form = forms.search_form()
-    return render(request, 'search_form.html', {'form'.form})
+def search_view(request):
+    if request.method == 'GET':
+        form = forms.search_form(request.POST)
+        if form.is_valid():
+            return render(request, artist_name, {'form':form})
+    else:
+        return HttpResponse('We could not locate that search.')
+

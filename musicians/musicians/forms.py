741745f0f334ba507a73756f25462aab226a4372
Jaclyn Mullin <jaclyn.mullin@gmail.com>
2017-11-13 13:45:59 -0500

    We have a search bar

--- musicians/musicians/forms.py
+++ musicians/musicians/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class search_form(forms.Form):
     artist_name = forms.CharField(label='Artist', max_length=100)
     band_name = forms.CharField(label='Band', max_length=100)
+
--- musicians/musicians/views.py
+++ musicians/musicians/views.py
@@ -8,16 +8,16 @@ def home(request):
     return render(request, 'home.html')
 
 
+# def search_form_view(request):
+#     if request.method == 'POST':
+#         form = forms.search_form(request.POST)
+#         if form.is_valid():
+#             return render(request, artist_name, {'form':form})
+#     else:
+#         return HttpResponse('We could not locate that search.')
+
+
 def search_form_view(request):
-    if request.method == 'POST':
-        form = forms.search_form(request.POST)
-        if form.is_valid():
-            return render(request, artist_name, {'form':form})
-    else:
-        return HttpResponse('We could not locate that search.')
-
-
-def do_search(request):
-    contacts = Contact.objects.filter(name__contains=request.GET['q'])
-    return render(request, 'results.html', {'contacts': contacts})
-
+    search_bar = SearchBar(request, ['name', 'age'])
+    if search_bar.is_valid():
+        my_name = search_bar['name']

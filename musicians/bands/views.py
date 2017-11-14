c5168ed260ef702c9983e7d2319c57433ac98176
Jaclyn Mullin <jaclyn.mullin@gmail.com>
2017-11-13 19:40:16 -0500

    working on views

--- musicians/bands/views.py
+++ musicians/bands/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from . models import Band
 
 
 def home(request):
     all_bands = Band.objects.all()
-    html = ''
-    for band in all_bands:
-        url = '/bands/' + str(band_id) + '/'
-        html += "<a herf = " ' + url + ' "> ' + band.band_name' </a> <br>"
-    return HttpResponse(html)
+    return render(request, 'band_list.html', {
+        'bands': all_bands,
+    })
+    # html = ''
+    # for band in all_bands:
+    #     url = '/bands/' + str(band.id) + '/'
+    #     html += "<a href = " ' + url + ' "> ' + band.band_name' </a> <br>"
+    # return HttpResponse(html)
 
 def band_detail(request, band_id):
-    return HttpResponse("<h2>Details for Band Id:" + str(band_id) + "</h2>")
+    get_band = Band.objects.get(id=band_id)
+    return render(request, 'BandDetail.html', {
+        'band_id': band_id,
+    })
